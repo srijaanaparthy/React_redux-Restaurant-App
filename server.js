6565566260fb65367c248cc013c1f91de94640c7
SrijaAnaparthy <86790568+SrijaAnaparthy@users.noreply.github.com>
2022-06-01 21:39:45 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const shortid = require('shortid');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const shortid = require("shortid");
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use("/", express.static(__dirname + "/build"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
 
 mongoose.connect("mongodb+srv://srijaanaparthy:12345@cluster0.6e0vh.mongodb.net/fooddata?retryWrites=true&w=majority",
 {
@@ -12,64 +15,84 @@ mongoose.connect("mongodb+srv://srijaanaparthy:12345@cluster0.6e0vh.mongodb.net/
     useUnifiedTopology : true,
 })
 
-var foodSchema = new mongoose.Schema
-(
+const Product = mongoose.model(
+  "products",
+  new mongoose.Schema({
+    _id: { type: String, default: shortid.generate },
+    title: String,
+    description: String,
+    image: String,
+    price: Number,
+    restaurant : String,
+    // availableSizes: [String],
+
+  })
+);
+
+app.get("/api/products", async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
+});
+
+app.post("/api/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+  res.send(deletedProduct);
+});
+
+const Order = mongoose.model(
+  "order",
+  new mongoose.Schema(
     {
-        _id : {type : String, default : shortid.generate},
-        title : String,
-        img : String,
-        price : String,
-        restaurant : String,
-        info : String
+      _id: {
+        type: String,
+        default: shortid.generate,
+      },
+      email: String,
+      name: String,
+      address: String,
+      total: Number,
+      cartItems: [
+        {
+          _id: String,
+          title: String,
+          price: Number,
+          count: Number,
+        },
+      ],
+    },
+    {
+      timestamps: true,
     }
-)
+  )
+);
 
-var fooddata = mongoose.model('item',foodSchema);
+app.post("/api/orders", async (req, res) => {
+  if (
+    !req.body.name ||
+    !req.body.email ||
+    !req.body.address ||
+    !req.body.total ||
+    !req.body.cartItems
+  ) {
+    return res.send({ message: "Data is required." });
+  }
+  const order = await Order(req.body).save();
+  res.send(order);
+});
+app.get("/api/orders", async (req, res) => {
+  const orders = await Order.find({});
+  res.send(orders);
+});
+app.delete("/api/orders/:id", async (req, res) => {
+  const order = await Order.findByIdAndDelete(req.params.id);
+  res.send(order);
+});
 
-app.get("/api/getdata",function(req,res)
-{
-    fooddata.find({},function(err,data)
-    {
-        console.log("mydata",data);
-        if(!err)
-        {  // console.log(data)
-            res.send(data);
-        }
-    })
-})
-
-// app.post("/api/items",async (req,res) =>
-// {
-//     console.log(req.body,"req body ......")
-//     const newItem = new fooddata(req.body)
-//     console.log(req.body,"req body")
-//     const savedItem = await newItem.save();
-//     res.send(savedItem)
-// })
-
-app.post("/api/addData", async (req, res) => {
-    console.log(req.body, 'req.body');
-    var newRec = new fooddata({
-      title : req.body.title,
-      img : req.body.img,
-      price : req.body.price,
-      restaurant : req.body.restaurant,
-      info : req.body.info
-    });
-    await newRec.save(function (err, newRecord) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send(newRecord);
-      }
-    });
-  });
-
-app.delete("/api/delete/:id",async (req,res) =>
-{   
-    const deleteItem = await fooddata.findByIdAndDelete(req.params.id)
-    res.send(deleteItem)
-})
-
-const port = process.env.PORT  || 5000;
-app.listen(port,()=> console.log("Server at http://localhost:5000"))
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("serve at http://localhost:5000"));
